76bcffed31d349c9fd258a89ead64d9a5bd6711f
AbVicario <abel.vicario.garcia@gmail.com>
2024-05-01 22:20:47 +0200

    [+] Hotfix

--- app/src/main/java/com/example/pataventura/data/ClienteRepository.kt
+++
@@ -1,4 +0,0 @@
-package com.example.pataventura.data
-
-class ClienteRepository {
-}
--- app/src/main/java/com/example/pataventura/data/model/Cuidador.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pataventura.data.model
-
-import com.google.gson.annotations.SerializedName
-
-data class Cuidador(
-    @SerializedName("nombre") val nombre: String
-)
